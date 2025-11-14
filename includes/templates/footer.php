adcaa319a0354137309c5c8173fc3ce592164cfd
Alex Mp <alexander135med@gmail.com>
2025-11-14 01:40:07 -0600

    Aplicar .gitignore y preparar push limpio

--- includes/templates/footer.php
+++ includes/templates/footer.php
@@ -5,15 +5,14 @@
                     <p>¿Nos quieres contar algo más?</p>
                 </label>
                 <textarea class='item2' id='comentario' name='comentario' rows='5' cols='30'
-                    placeholder='Escribe aquí tus comentarios'></textarea>
-                <br>
+                    placeholder='Escribe aquí tus dudas'></textarea>
                 <button class='boton' type='submit'>Enviar</button>
             </form>
 
             <p class='item2 sincursor'>Todos los derechos reservados Vive Composta <?php echo date('Y'); ?> &copy;</p>
             <div class='item3'>
                 <p><a href='/Aviso_VC.pdf' target='_blank'>Aviso de privacidad</a></p>
-                <p><a href='#'>Preguntas frecuentes</a></p>
+                <p><a href='/Preguntas frecuentes.pdf' target="_blank">Preguntas frecuentes</a></p>
                 <p><a href='#'>Términos y condiciones</a></p>
             </div>
 
